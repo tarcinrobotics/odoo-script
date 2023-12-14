46ea85f44d43135ea7ac40db309217d62e05932d
tarcinrobotics <devops@tarcinacademy.in>
2023-12-14 11:34:47 +0530

    service file has been completed

--- install.sh
+++ install.sh
@@ -4,8 +4,54 @@
 ################################################################################
 #!/bin/bash
 # declaration of variables for location storage
-conf_file = "/etc/odoo.conf"
-service_file = "/etc/systemd/system/odoo.service"
+conf_file="/etc/odoo.conf"
+service_file="/etc/systemd/system/odoo.service"
+
+config_content="
+[options]
+
+; Database operations password:
+
+admin_passwd = odoo
+
+db_host = localhost
+
+db_port = 5432
+
+db_user = odoo
+
+db_password = odoo
+
+addons_path = /opt/odoo/odoo/addons,/opt/odoo/odoo-custom-addons"
+
+service_content="[Unit]
+
+Description=Odoo
+
+Requires=postgresql.service
+
+After=network.target postgresql.service
+
+[Service]
+
+Type=simple
+
+SyslogIdentifier=odoo
+
+PermissionsStartOnly=true
+
+User=odoo
+
+Group=odoo
+
+ExecStart=/opt/odoo/odoo-venv/bin/python3 /opt/odoo/odoo/odoo-bin -c /etc/odoo.conf
+
+StandardOutput=journal+console
+
+[Install]
+
+WantedBy=multi-user.target"
+
 
 # updating server 
 echo -e "\n---- Updating Server ----\n"
@@ -141,7 +187,7 @@ if exit ; then
     echo -e "\n---- exited from odoo user !!! ----\n"
 else
     echo -e "\n---- exiting from odoo user failed !!! ----\n"
-fi
+fi 
 EOF
 # creating odoo.conf file 
 
@@ -154,71 +200,23 @@ fi
 
 
 # adding contents to the file
-if cat <<EOF >"$conf_file"
-
-[options]
-
-; Database operations password:
-
-admin_passwd = odoo
-
-db_host = localhost
-
-db_port = 5432
-
-db_user = odoo
-
-db_password = odoo
-
-addons_path = /opt/odoo/odoo/addons,/opt/odoo/odoo-custom-addons
-EOF ; then
+if echo "$config_content" > "$config_file" ; then
     echo -e "\n---- config file created successfully ----\n"
 else
     echo -e "\n---- failed to create odoo.conf file ----\n"
 fi
 
 # creating odoo service
-
 if  sudo touch /etc/systemd/system/odoo.service ; then
     echo -e "\n---- odoo system service file has been created !!! ----\n"
 else
     echo -e "\n---- odoo system service file creation has been failed / file already exists!!! ----\n"
-
 fi
 
 
 # adding contents to the file
 
-if cat <<EOF >"$service_file"  
-
-[Unit]
-
-Description=Odoo
-
-Requires=postgresql.service
-
-After=network.target postgresql.service
-
-[Service]
-
-Type=simple
-
-SyslogIdentifier=odoo
-
-PermissionsStartOnly=true
-
-User=odoo
-
-Group=odoo
-
-ExecStart=/opt/odoo/odoo-venv/bin/python3 /opt/odoo/odoo/odoo-bin -c /etc/odoo.conf
-
-StandardOutput=journal+console
-
-[Install]
-
-WantedBy=multi-user.target
-EOF ; then
+if echo "$service_content" > "$service_file"  ; then
         echo -e "\n---- service file created successfully ----\n"
 else
         echo -e "\n---- failed to create service file ----\n"
