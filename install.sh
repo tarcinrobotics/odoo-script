c06a49eaa443efc80007eca8577b602cffe47e8d
vignesh-pandian-p <vigneshpandian0710@gmail.com>
2024-06-12 16:00:02 +0530

    made changes in ref according to nirvagi

--- install.sh
+++ install.sh
@@ -5,29 +5,29 @@
 #!/bin/bash
 # declaration of variables for location storage
 
-conf_file="/etc/odoo.conf"
-service_file="/etc/systemd/system/odoo.service"
+conf_file="/etc/nirvagi.conf"
+service_file="/etc/systemd/system/nirvagi.service"
 
 config_content="[options]
-admin_passwd=odoo
+admin_passwd=nirvagi
 db_host=False
 db_port=False
-db_user=odoo
+db_user=nirvagi
 db_password=False
-addons_path=/opt/odoo17/odoo/addons,/opt/odoo17/odoo/odoo-custom-addons
+addons_path=/opt/nirvagi/addons,/opt/nirvagi/custom-addons
 xmlrpc_port=8069"
 
 service_content="[Unit]
-Description=Odoo
+Description=
 Requires=postgresql.service
 After=network.target postgresql.service
 [Service]
 Type=simple
 SyslogIdentifier=odoo
 PermissionsStartOnly=true
-User=odoo
-Group=odoo
-ExecStart=/opt/odoo/odoo-venv/bin/python3 /opt/odoo17/odoo/odoo-bin -c /etc/odoo.conf
+User=nirvagi
+Group=nirvagi
+ExecStart=/opt/nirvagi/nirvagi-venv/bin/python3 /opt/nirvagi/odoo-bin -c /etc/nirvagi.conf
 StandardOutput=journal+console
 [Install]
 WantedBy=multi-user.target"
@@ -46,33 +46,33 @@ else
 fi
 
 # creating service and config files for odoo
-echo "---- CREATING CONFIG AND SERVICE FILES FOR ODOO ----"
+echo "---- CREATING CONFIG AND SERVICE FILES FOR NIRVAGI ----"
 # service file
-if sudo touch /etc/systemd/system/odoo.service ; then
-    echo "---- odoo.service file has been created !!! ----"
+if sudo touch /etc/systemd/system/nirvagi.service ; then
+    echo "---- nirvagi.service file has been created !!! ----"
 else
-    echo "---- failed to create the odoo.service file !!! ----"
+    echo "---- failed to create the nirvagi.service file !!! ----"
 fi
 
 # conf file
-if sudo touch /etc/odoo.conf ; then 
-    echo "---- odoo.conf file has been created !!! ----"
+if sudo touch /etc/nirvagi.conf ; then 
+    echo "---- nirvagi.conf file has been created !!! ----"
 else
-    echo "---- failed to create the odoo.conf file ----"
+    echo "---- failed to create the nirvagi.conf file ----"
 fi
 # creating and giving permission directories
 echo "---- GIVING PERMISSIONS ----"
 # creating dir
-if sudo mkdir -p /opt/odoo17/odoo ; then
-    echo "---- odoo directory has been created ----"
+if sudo mkdir -p /opt/nirvagi ; then
+    echo "---- nirvagi directory has been created ----"
 else
     echo "---- Failed to create the odoo directory / directory already exists !!! ----"
 fi
 # giving permission to dir
-if sudo chmod u+w /opt/odoo17 ; then
-    echo "---- successfully given permission to odoo directory ----"
+if sudo chmod u+w /opt/nirvagi ; then
+    echo "---- successfully given permission to nirvagi directory ----"
 else
-    echo "---- Failed to create the odoo directory !!! ----"
+    echo "---- Failed to create the nirvagi directory !!! ----"
 fi
 
 # installing pre-requisites packages 
@@ -82,25 +82,25 @@ echo    "---- completed installing pre-requisites !!! ----"
 
 # creating odoo user
 
-echo    "---- Creating Odoo user ----"
+echo    "---- Creating nirvagi user ----"
 
-if sudo useradd -m -d /opt/odoo -U -r -s /bin/bash odoo ; then
-    echo    "---- completed creating odoo user  !!! ----"
+if sudo useradd -m -d /opt/nirvagi -U -r -s /bin/bash nirvagi ; then
+    echo    "---- completed creating nirvagi user  !!! ----"
 else
-    echo "odoo user creation failed / the user already exists"
+    echo "nirvagi user creation failed / the user already exists"
 fi
 
 # giving permission to odoo user
-if sudo chown -R odoo /opt/odoo17 ; then
-    echo "---- giving permission to odoo user ----"
+if sudo chown -R nirvagi /opt/nirvagi ; then
+    echo "---- giving permission to nirvagi user ----"
 else
-    echo "---- Failed to give permission to odoo user ----"
+    echo "---- Failed to give permission to nirvagi user ----"
 fi
 # creating postgres sql user for odoo
 
 echo    "---- Creating Postgresql user for user ----"
 
-if sudo su - postgres -c 'createuser -s odoo'; then
+if sudo su - postgres -c 'createuser -s nirvagi'; then
     echo    "---- completed creating postgresql user !!! ----"
 else
     echo    "postgersql user creation failed !!!"
@@ -119,24 +119,24 @@ fi
 
 # installation and configuration of odoo
 
-echo    "---- SWITCHING TO ODOO USER ----"
-sudo su - odoo  <<EOF
+echo    "---- SWITCHING TO NIRVAGI USER ----"
+sudo su - nirvagi  <<EOF
 
 
 echo    "---- cloning from github ----"
 
 
-if git clone https://github.com/tarcinrobotics/nirvagi-dev --depth 1 --branch main /opt/odoo17/odoo ; then 
+if git clone https://github.com/tarcinrobotics/nirvagi-dev --depth 1 --branch main /opt/nirvagi ; then 
     echo    "---- successfully cloned from github !!! ----"
 else
     echo    "---- failed cloning from github / cloned files already present !!! ----"
 fi
 
 # configuring odoo
-cd /opt/odoo
-python3 -m venv odoo-venv
+cd /opt/nirvagi
+python3 -m venv nirvagi-venv
 echo    "---- activating virtual environment "
-if source /opt/odoo/odoo-venv/bin/activate ; then
+if source /opt/nirvagi/nirvagi-venv/bin/activate ; then
     echo "---- virtual environment activated successfully !!!"
 else
     echo "---- virtual environment failed !!!"
@@ -148,7 +148,7 @@ fi
 pip3 install wheel
 
 # installing odoo-requirements.txt file
-if pip3 install -r /opt/odoo17/odoo/requirements.txt ; then
+if pip3 install -r /opt/nirvagi/requirements.txt ; then
     echo "---- requirements installed successfully !!!"
 else
     echo "---- requirements installation failed !!!"
@@ -167,7 +167,7 @@ fi
 # creating custom-addons
 
 echo    "---- creating custom-addons directory----"
-if mkdir /opt/odoo/odoo-custom-addons ; then
+if mkdir /opt/nirvagi/custom-addons ; then
     echo    "---- custom-addons directory has been created successfully !!! ----"
 else
     echo    "---- directory creation failed / directory already available !!!"
@@ -190,7 +190,7 @@ echo "---- WRITING CONFIG AND SERVICE FILES ----"
 if  echo "$config_content" | sudo tee "$conf_file" > /dev/null ; then
     echo    "---- config file created successfully ----"
 else
-    echo    "---- failed to create odoo.conf file ----"
+    echo    "---- failed to create nirvagi.conf file ----"
 fi
 
 # adding contents to the service file
@@ -201,14 +201,14 @@ else
         echo    "---- failed to create service file ----"
 fi
 
-echo "---- writing completed !!!! ----"
+echo "---- file writing completed !!!! ----"
 # starting odoo 
 
 echo "---- STARTING ODOO SERVICE ----"
-if sudo systemctl enable --now odoo ; then
-    echo    "---- Odoo service has been added in startup----"
+if sudo systemctl enable --now nirvagi ; then
+    echo    "---- Nirvagi service has been added in startup----"
 else
-    echo    "---- odoo service failed in adding in startup----"
+    echo    "---- Nirvagi service failed in adding in startup----"
 fi
 
 if sudo systemctl daemon-reload ; then
@@ -217,10 +217,10 @@ else
     echo "--- failed to reload daemon !!! ---"
 fi
 
-if sudo systemctl start odoo.service ; then
-    echo "--- odoo service started successfully !!! ---"
+if sudo systemctl start nirvagi.service ; then
+    echo "--- nirvagi service started successfully !!! ---"
 else
-    echo "--- failed to start odoo service !!! ---"
+    echo "--- failed to start nirvagi service !!! ---"
 fi
 
-echo "odoo17 has been installed successfully !!! "
+echo "*** nirvagi has been installed successfully !!! ***"
